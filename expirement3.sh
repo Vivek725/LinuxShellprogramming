47731c470f7815fa45f73073e8b4da2510623f19
Vivek Singh <vivek.51-cse-19@mietjammu.in>
2022-12-22 23:25:36 +0530

    experiment 3

--- expirement3.sh
+++ expirement3.sh
@@ -2,4 +2,4 @@
 echo "Write a shell script to see current date, time username and directory"
 echo "Today's date is: `date`"
 echo "Username is `whoami`"
-echo "The current directory is `pwd`"
+echo "The current directory is  `pwd`"
